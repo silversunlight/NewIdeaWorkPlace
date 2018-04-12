c2bc5c134df4cc8ce988c9465e07d55793e61693
sunlang <sunlight328@163.com>
2018-04-12 16:43:49 +0800

    just save

--- SpringLearning/src/main/java/interfaces/beanLifecycle/BeanLifecycle.java
+++ SpringLearning/src/main/java/interfaces/beanLifecycle/BeanLifecycle.java
@@ -1,11 +1,31 @@
 package interfaces.beanLifecycle;
 
-public class BeanLifecycle {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifecycle implements InitializingBean,DisposableBean {
+
+    public void defaultInit() {
+        System.out.println("default init");
+    }
+
+    public void defaultDestroy() {
+        System.out.println("default destroy");
+    }
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitialisingBean init");
+    }
     public void start() {
-        System.out.println("start");
+        System.out.println("initMethod start");
     }
 
     public void stop() {
-        System.out.println("stop");
+        System.out.println("destroyMethod start");
     }
 }
